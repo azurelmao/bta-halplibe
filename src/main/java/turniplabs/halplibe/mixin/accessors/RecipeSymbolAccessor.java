54e507847bc0e2712daf0af4976df46027848787
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2024-04-11 20:03:38 +0200

    Fix warning.

--- src/main/java/turniplabs/halplibe/mixin/accessors/RecipeSymbolAccessor.java
+++ src/main/java/turniplabs/halplibe/mixin/accessors/RecipeSymbolAccessor.java
@@ -4,7 +4,7 @@ import net.minecraft.core.data.registry.recipe.RecipeSymbol;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(RecipeSymbol.class)
+@Mixin(value = RecipeSymbol.class, remap = false)
 public interface RecipeSymbolAccessor {
     @Accessor
     void setSymbol(char symbol);
